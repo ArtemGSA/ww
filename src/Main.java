1c5448de1a92f551f35c2c9ac2f9e4a196f4d8bd
ArtemGSA <ArtemGSA@yandex.ru>
2024-03-09 10:45:37 +0300

    q

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,38 @@ public class Main {
     //return arr;
     //}
 
+    public static void tusk1() {
+        //ЗАДАЧА1
+        System.out.println("ЗАДАЧА1");
+        int[] arr1 = {1,2,3,4,5};
+        int summ = 0;
+        for (int i : arr1){
+            summ+=i;
+        }
+        System.out.println("Cумма трат за месяц составила " + summ + " рублей");
+        //ЗАДАЧА2
+        System.out.println("ЗАДАЧА2");
+        int min = 2000000;
+        int max = -1;
+        for (int i : arr1){
+            if(i>max){
+                max = i;
+            }
+            if(i<min){
+                min = i;
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила "+min+ " рублей. Максимальная сумма трат за неделю составила "+max+" рублей");
+        //ЗАДАЧА3
+        System.out.println("ЗАДАЧА3");
+        int kolvoDney = 30;
+        int sredTrat = summ/kolvoDney;
+        System.out.println("Средняя сумма трат за месяц составила " + sredTrat+ " рублей ");
+        //ЗАДАЧА4
+        System.out.println("ЗАДАЧА4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1; i>=0; i--){
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
